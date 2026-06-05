bbc454beab694203a26ca4c760b85c21908a7c1b
Nano <alhernano@gmail.com>
2026-06-05 15:49:54 -0500

    Added some refactor on config.org

--- init.el
+++ init.el
@@ -301,13 +301,17 @@
 (use-package org
   :ensure nil
   :custom
-  (org-edit-src-content-indentation 4)
+  (org-edit-src-content-indentation 4) ;; Set src block automatic indent to 4 instead of 2.
+
   :hook
-  (org-mode . org-indent-mode)
-  (org-mode . (lambda ()
+  (org-mode . org-indent-mode) ;; Indent text
+;;   The following prevents <> from auto-pairing when electric-pair-mode is on.
+;;  Otherwise, org-tempo is broken when you try to <s TAB...
+ (org-mode . (lambda ()
                 (setq-local electric-pair-inhibit-predicate
                             `(lambda (c)
-                               (if (char-equal c ?<) t (,electric-pair-inhibit-predicate c)))))))
+                               (if (char-equal c ?<) t (,electric-pair-inhibit-predicate c))))))
+  )
 
 (use-package toc-org
   :commands toc-org-enable
